dcab3d91a0eb5ec179e7ecf69ca914d2143bddbd
Paul Leone <pnleone@outlook.com>
2025-08-29 07:20:41 -0400

    added Terraform and ignore file

---
+++ Terraform Configs/proxmox_lxc.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "Terraform-for-Proxmox/proxmox"
+      version = "3.0.2-rc04"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url            = "https://pve.home.com:8006/api2/json"
+  pm_api_token_id       = "terraform@pam!terraform"
+  pm_api_token_secret   = "733fd8be-7e7e-4cfd-b8fb-fe178326b4f6"
+  pm_tls_insecure       = false  # true only for self-signed lab certs
+}
+
+resource "proxmox_lxc" "testct" {
+  hostname     = "tf-demo-ct"
+  target_node  = "pve"
+  ostemplate   = "local:vztmpl/debian-12-standard_12.2-1_amd64.tar.zst"
+  password     = "changeme"
+  cores        = 2
+  memory       = 1024
+  swap         = 512
+  onboot       = true
+  start        = true
+
+  rootfs {
+    storage = "local-lvm"
+    size    = "8G"
+  }
+
+  network {
+    name   = "eth0"
+    bridge = "vmbr0"
+    ip     = "dhcp"
+  }
+
+  features {
+    nesting = true
+  }
+}
---
+++ Terraform Configs/proxmox_vmv2.tf
@@ -0,0 +1,41 @@
+terraform {
+    required_providers {
+        proxmox = {
+            source = "telmate/proxmox"
+        }
+    }
+}
+
+provider "proxmox" {
+    pm_api_url          = "https://pve.home.com:8006/api2/json"
+    pm_api_token_id     = "terraform@pam!terraform"
+    pm_api_token_secret = "733fd8be-7e7e-4cfd-b8fb-fe178326b4f6"
+    pm_tls_insecure     = false
+}
+
+resource "proxmox_vm_qemu" "ubuntu-terratest" {
+    name                = "ubuntu-terratest"
+    target_node         = "pve"
+    clone               = "ubuntu-cloud"
+    full_clone          = true
+    cores               = 2
+    memory              = 2048
+    sockets             = 1
+    onboot              = true
+    agent               = 1
+    
+    disk {
+        size            = "32G"
+        type            = "scsi"
+        storage         = "local-lvm"
+        discard         = "on"
+    }
+
+    network {
+        model     = "virtio"
+        bridge    = "vmbr0"
+        firewall  = false
+        link_down = false
+    }
+
+}
